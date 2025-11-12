060d3c7e1ea177c08190f5e189ac7fe69c49576e
Eray Emir <128998801+peartolsamia@users.noreply.github.com>
2025-11-12 19:10:06 +0300

    Main menu buttons changed and brick not despawning on client problem fixed

--- Assets/_Scripts/Brick/Brick.cs
+++ Assets/_Scripts/Brick/Brick.cs
@@ -1,18 +1,45 @@
 using UnityEngine;
 using Unity.Netcode;
 
+[RequireComponent(typeof(NetworkObject))]
 public class Brick : NetworkBehaviour
 {
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Ball"))
+        if (!collision.collider.CompareTag("Ball"))
+            return;
+
+        
+        bool hasNO = TryGetComponent<NetworkObject>(out var no);
+
+        
+        Debug.Log($"[BRICK] IsServer={IsServer}, HasNO={hasNO}, IsSpawned={(hasNO && no.IsSpawned)} NetId={(hasNO ? no.NetworkObjectId : 0)}");
+
+        
+        if (!IsServer)
+            return;
+
+        
+        if (hasNO && no.IsSpawned)
         {
+            no.Despawn(true);  
+        }
+        else
+        {
+            
             Destroy(gameObject);
         }
-
     }
 
+    public override void OnNetworkSpawn()
+    {
+        Debug.Log($"[BRICK] OnNetworkSpawn side={(IsServer ? "SERVER" : "CLIENT")} " +
+                  $"hasNO={TryGetComponent<NetworkObject>(out var no)} spawned={(no && no.IsSpawned)} id={(no ? no.NetworkObjectId : 0)}");
+    }
 
-
-
+    public override void OnNetworkDespawn()
+    {
+        Debug.Log($"[BRICK] OnNetworkDespawn side={(IsServer ? "SERVER" : "CLIENT")}, name={name}");
+        Destroy(gameObject);
+    }
 }
